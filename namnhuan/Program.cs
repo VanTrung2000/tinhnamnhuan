10c8c8ab4179d73584c22cf643b3f4a7d270fd2d
VanTrung2000 <vantrungdeptrai2000@gmail.com>
2020-09-08 21:15:27 +0700

    Add project files.

---
+++ namnhuan/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace namnhuan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nam;
+    
+            Console.Write("Nhap mot nam bat ky: ");
+            nam = Convert.ToInt32(Console.ReadLine());
+
+            if ((nam % 400) == 0)
+                Console.WriteLine("la nam nhuan.", +nam);
+            else if ((nam % 100) == 0)
+                Console.WriteLine(" khong phai la nam nhuan.", +nam);
+            else if ((nam % 4) == 0)
+                Console.WriteLine(" la nam nhuan.", +nam);
+            else
+                Console.WriteLine(" khong phai la nam nhuan.",+nam); 
+
+            Console.ReadKey();
+
+        }
+    }
+}
