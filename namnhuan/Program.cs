9ecd3e03bfba5a673fe9d34dc8df63b27cdb2fb0
VanTrung2000 <70925652+VanTrung2000@users.noreply.github.com>
2020-09-09 20:14:27 +0700

    Update Program.cs

--- namnhuan/Program.cs
+++ namnhuan/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace namnhuan
 {
@@ -6,11 +6,15 @@ namespace namnhuan
     {
         static void Main(string[] args)
         {
+
             int nam;
-    
+
             Console.Write("Nhap mot nam bat ky: ");
             nam = Convert.ToInt32(Console.ReadLine());
 
+            namnhuan(nam);
+        }
+        static int namnhuan(int nam){
             if ((nam % 400) == 0)
                 Console.WriteLine("la nam nhuan.", +nam);
             else if ((nam % 100) == 0)
@@ -18,10 +22,10 @@ namespace namnhuan
             else if ((nam % 4) == 0)
                 Console.WriteLine(" la nam nhuan.", +nam);
             else
-                Console.WriteLine(" khong phai la nam nhuan.",+nam); 
+                Console.WriteLine(" khong phai la nam nhuan.", +nam);
 
             Console.ReadKey();
-
+            return nam;
         }
     }
 }
